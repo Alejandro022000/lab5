ef16b1c827d1fe6c556003b77e1698829cb0c96f
Alejandro Perez Gonzalez <ale07pg@gmail.com>
2022-11-09 08:18:27 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <fstream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include "ArgumentManager.h"
+
+using namespace std;
+
+string removePunct(string w){
+  string word;
+  for(int i = 0; i < w.size(); i++){
+    if(w[i] == '\'' || isalpha(w[i]) || isdigit(w[i]))
+      word += w[i];
+  }
+  return word;
+}
+
+int main(int argc, char *argv[]) {
+
+  ArgumentManager am(argc, argv);
+  ifstream input(am.get("input"));
+  ofstream output(am.get("output"));
+
+/*
+ifstream input("input3.txt");
+ofstream output("output.txt");
+*/
+  
+map<string, int> hmap;
+string word;
+
+while(input >> word){
+  word = removePunct(word);
+
+  transform(word.begin(), word.end(), word.begin(), ::tolower);
+  
+    if(hmap.find(word) == hmap.end()){
+        hmap.insert({word, 1});
+    }
+
+    else {
+        for (auto x :  hmap){
+            if(word == x.first){
+                hmap[word]++;
+            }
+        }
+    }
+
+}
+
+  for(auto x = hmap.begin(); x != hmap.end(); x++){
+    output << x->first << ": " << x->second << endl;
+  }
+  
+return 0;
+}//end main

9d4b5b4cfb18e8e41fd5680c37c7af1eb630d283
Alejandro Perez Gonzalez <ale07pg@gmail.com>
2022-11-09 11:03:44 -0600

    final commit

--- main.cpp
+++ main.cpp
@@ -3,54 +3,58 @@
 #include <fstream>
 #include <algorithm>
 #include "ArgumentManager.h"
-
 using namespace std;
 
-string removePunct(string w){
-  string word;
-  for(int i = 0; i < w.size(); i++){
-    if(w[i] == '\'' || isalpha(w[i]) || isdigit(w[i]))
-      word += w[i];
-  }
-  return word;
+string delete_punctuaction(string);
+void to_lower(string &single_word){
+  for (int x=0; x<single_word.size(); x++)
+        single_word[x] = tolower(single_word[x]);
 }
 
 int main(int argc, char *argv[]) {
+string single_word;
+map<string, int> dict;
 
-  ArgumentManager am(argc, argv);
-  ifstream input(am.get("input"));
-  ofstream output(am.get("output"));
-
+ArgumentManager am(argc, argv);
+ifstream input(am.get("input"));
+ofstream output(am.get("output"));
 /*
 ifstream input("input3.txt");
 ofstream output("output.txt");
 */
+while(input >> single_word){
+  single_word = delete_punctuaction(single_word);
+  to_lower(single_word);  
   
-map<string, int> hmap;
-string word;
-
-while(input >> word){
-  word = removePunct(word);
-
-  transform(word.begin(), word.end(), word.begin(), ::tolower);
-  
-    if(hmap.find(word) == hmap.end()){
-        hmap.insert({word, 1});
-    }
-
-    else {
-        for (auto x :  hmap){
-            if(word == x.first){
-                hmap[word]++;
-            }
-        }
-    }
+  if(dict.find(single_word) == dict.end()){
+      dict.insert({single_word, 1});
+  }
+  else {
+      for (auto x :  dict){
+          if(single_word == x.first){
+              dict[single_word]++;
+          }
+      }
+  }
 
 }
 
-  for(auto x = hmap.begin(); x != hmap.end(); x++){
+  for(auto x = dict.begin(); x != dict.end(); x++){
+    cout << x->first << ": " << x->second << endl;
     output << x->first << ": " << x->second << endl;
   }
   
 return 0;
-}//end main
+}
+
+
+string delete_punctuaction(string temp_word){
+  string single_word;
+  for(int i = 0; i < temp_word.size(); i++){
+    if(isalpha(temp_word[i])|| temp_word[i] == '\''){
+      single_word += temp_word[i];
+    }
+  }
+  return single_word;
+}
+
